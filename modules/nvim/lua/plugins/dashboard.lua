782cfef9a5b0be0e300e3a19e45f98c1ba3b65e9
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-05-28 21:28:54 -0500

    feat(nvim): adds ability to disable dashboard buttons
    configures dashboard buttons to open github repo if it exists and
    dynamically toggle the dotfiles and obsidian vault buttons

--- modules/nvim/lua/plugins/dashboard.lua
+++ modules/nvim/lua/plugins/dashboard.lua
@@ -1,14 +1,42 @@
 -- alpha-nvim
 -- https://github.com/goolord/alpha-nvim
 
+local isInsideTmux = function()
+	return vim.env.TMUX ~= nil
+end
+
+local isGitHubRepo = function()
+	local git_dir = vim.loop.cwd() .. "/.git"
+	return vim.loop.fs_stat(git_dir) ~= nil
+end
+
+local cwdMatchesEnv = function(variable)
+	local envRepoDirectory = vim.fn.resolve(vim.env[variable])
+	return vim.loop.cwd() == envRepoDirectory
+end
+
 local buttons = {
+	-- File actions
 	{ "r", "   [R]ecent", ":Telescope oldfiles <CR>" },
 	{ "e", "   N[e]w file", ":ene <BAR> startinsert <CR>" },
-	{ "c", "   [C]onfigure Dotfiles", ":!tmux-sessionizer $(readlink -f $DOTFILES) <CR>" },
-	{ "u", "󰚰   [U]pdate Plugins", "<cmd>lua require('lazy').sync() <CR>" },
 	{ "f", "󰱽   Open [f]ile", ":Telescope find_files <CR>" },
 	{ "g", "   [G]rep files", ":Telescope live_grep <CR>" },
-	{ "v", "󱓩   Open Obsidian [V]ault", ":!tmux-sessionizer $OBSIDIAN_DIRECTORY <CR>" },
+	{ enabled = isGitHubRepo(), "o", "   [O]pen GitHub Repo", ":!git open <CR>" },
+	-- Open common nvim projects
+	{
+		enabled = not cwdMatchesEnv("DOTFILES") and isInsideTmux(),
+		"c",
+		"   [C]onfigure Dotfiles",
+		":!tmux-sessionizer $(readlink -f $DOTFILES) <CR>",
+	},
+	{
+		enabled = not cwdMatchesEnv("OBSIDIAN_DIRECTORY") and isInsideTmux(),
+		"v",
+		"󱓩   Open Obsidian [V]ault",
+		":!tmux-sessionizer $OBSIDIAN_DIRECTORY <CR>",
+	},
+	-- Nvim controls
+	{ "u", "󰚰   [U]pdate Plugins", "<cmd>lua require('lazy').sync() <CR>" },
 	{ "d", "   [D]ocs Search", ":Telescope help_tags <CR>" },
 	{ "x", "   E[x]it NVIM", ":qa <CR>" },
 }
@@ -39,8 +67,10 @@ return {
 		}
 		dashboard.section.buttons.val = {}
 		for i in pairs(buttons) do
-			local key, description, command = buttons[i][1], buttons[i][2], buttons[i][3]
-			table.insert(dashboard.section.buttons.val, dashboard.button(key, description, command))
+			local key, description, command, enabled = buttons[i][1], buttons[i][2], buttons[i][3], buttons[i].enabled
+			if enabled ~= false then
+				table.insert(dashboard.section.buttons.val, dashboard.button(key, description, command))
+			end
 		end
 		math.randomseed(os.time())
 		dashboard.section.footer.val = footers[math.random(#footers)]
